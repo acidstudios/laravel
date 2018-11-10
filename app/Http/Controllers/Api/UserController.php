cc6fed5e989352c8516f6dfb04ff94d9fda695a3
Gustavo de Jesús Barrientos Guerrero <gustavo.barrientos@acidstudios.me>
2018-11-09 19:17:16 -0600

    Initial commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request; 
+use App\Http\Controllers\Controller; 
+use App\User; 
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Login user and create access token
+     * @param [string] email
+     * @param [string] password
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('AuthorizationToken');
+        $token = $tokenResult->token;
+
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => $tokenResult->token->expires_at
+        ]);
+    }
+}
