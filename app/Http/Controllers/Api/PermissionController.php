8f17b7c4cd29b1c3fd5803935a322cced2b08592
Gustavo de Jesús Barrientos Guerrero <gustavo.barrientos@acidstudios.me>
2018-12-28 16:21:42 -0600

    Permission and Role controller added

--- app/Http/Controllers/Api/PermissionController.php
+++ app/Http/Controllers/Api/PermissionController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller; 
 use App\Permission;
 use Illuminate\Http\Request;
+use Validator;
 
 class PermissionController extends Controller
 {
