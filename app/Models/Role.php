192bfd655f3052c0f581f5499774161208a25fa2
Gustavo de Jesús Barrientos Guerrero <gustavo.barrientos@acidstudios.me>
2018-12-29 17:54:11 -0600

    Role model added fillable properties

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'slug', 'name'
+    ];
+    
     public function permissions() {
         return $this->belongsToMany(Permission::class, 'roles_permissions');
     }
